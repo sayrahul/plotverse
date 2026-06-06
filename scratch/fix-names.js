f4bc6db31cd9ed3f75cd9faf646bac5d28b0f8c7
Rahul Jadhav <rahuljadhav44@gmail.com>
2026-06-06 09:03:07 +0530

    Fix plot number mismatches and add 123, 183

---
+++ scratch/fix-names.js
@@ -0,0 +1,17 @@
+﻿const fs = require('fs');
+let svg = fs.readFileSync('public/data/plot.svg', 'utf8');
+
+svg = svg.replace(/<g([^>]+data-name="([^"]+)"[^>]*)>([\s\S]*?)<\/g>/g, (match, gAttrs, dataName, inner) => {
+  const textMatch = inner.match(/<text[^>]*>[\s\S]*?<tspan[^>]*>(\d+)<\/tspan>[\s\S]*?<\/text>/);
+  if (textMatch) {
+    const textNumber = textMatch[1];
+    if (dataName !== textNumber) {
+      console.log("Fixing mismatch: data-name=" + dataName + " -> " + textNumber + "");
+      const newAttrs = gAttrs.replace("data-name=\"" + dataName + "\"", "data-name=\"" + textNumber + "\"");
+      return "<g" + newAttrs + ">" + inner + "<\/g>";
+    }
+  }
+  return match;
+});
+
+fs.writeFileSync('public/data/plot.svg', svg);
